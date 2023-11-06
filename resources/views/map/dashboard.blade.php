eaa2531da0cedab733c1704c081902dfb89c9a97
MercuryMat <mateo252003@gmail.com>
2023-11-05 21:35:46 -0500

    estilos

--- resources/views/map/dashboard.blade.php
+++ resources/views/map/dashboard.blade.php
@@ -1,16 +1,17 @@
 @extends('partials.main')
 
 @section('head')
+    <link rel="stylesheet" href="../../../public/css/vmap.css" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 @endsection
 
 @section('content')
-    <div id="app">
-        <div class="section-main">
+    <div class="hidromap" id="app">
+        <div class="map-main">
             <h1>HIDROMAP</h1>
         </div>
-        <div id="map" style="width: 80%; height: 600px;"></div>
+        <div class="vmap" id="map" style="width: 100%; height: 93vh;"></div>
     </div>
 @endsection
 
